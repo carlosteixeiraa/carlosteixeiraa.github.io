3166b71e3a24b2d5fac2c73d76e279d5c7429b9c
carlosteixeiraa <carlosteixeira.email@gmail.com>
2018-01-28 17:22:41 +0000

    responsivo feito

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,65 @@
-// Animate image and text of home page
-$(document ).ready(() => {
-    $('.me').addClass('magictime puffIn');
-    $('aside h1').addClass('magictime slideLeftReturn');
-    $('.about').addClass('magictime slideRightReturn');
-});	
+var screenwidth= $(window).width();
 
-// Skills menu button
-$("#skills").on("click", () => {
-    $("#h-container").css("display","none")
-    $("#c-container").css("display","none")
-    $("#s-container").css("display","flex")
-    $('#s-container h1').addClass('magictime slideLeftReturn');
-    $('#s-container p').addClass('magictime slideRightReturn');
-});
+if (screenwidth > 480) {
+    // Animate image and text of home page
+    $(document ).ready(() => {
+        $('.me').addClass('magictime puffIn');
+        $('aside h1').addClass('magictime slideLeftReturn');
+        $('.about').addClass('magictime slideRightReturn');
+    });	
 
-// Home menu button
-$("#home").on("click", () => {
-    $("#h-container").css("display","flex")
-    $("#c-container").css("display","none")
-    $("#s-container").css("display","none")
-});
+    // Skills menu button
+    $("#skills").on("click", () => {
+        $("#h-container").css("display","none")
+        $("#c-container").css("display","none")
+        $("#s-container").css("display","flex")
+        $('#s-container h1').addClass('magictime slideLeftReturn');
+        $('#s-container p').addClass('magictime slideRightReturn');
+    });
 
-// Contact me menu button
-$("#contact").on("click", () => {
-    $("#h-container").css("display","none")
-    $("#c-container").css("display","flex")
-    $("#s-container").css("display","none")
-    $('#c-container p').addClass('magictime puffIn');
-});
+    // Home menu button
+    $("#home").on("click", () => {
+        $("#h-container").css("display","flex")
+        $("#c-container").css("display","none")
+        $("#s-container").css("display","none")
+    });
+
+    // Contact me menu button
+    $("#contact").on("click", () => {
+        $("#h-container").css("display","none")
+        $("#c-container").css("display","flex")
+        $("#s-container").css("display","none")
+        $('#c-container p').addClass('magictime puffIn');
+    });
+} else {
+    // Animate image and text of home page
+    $(document ).ready(() => {
+        $('.me').addClass('magictime puffIn');
+        $('aside h1').addClass('magictime slideLeftReturn');
+        $('.about').addClass('magictime slideRightReturn');
+    });	
+
+    // Skills menu button
+    $("#skills").on("click", () => {
+        $("#h-container").css("display","none")
+        $("#c-container").css("display","none")
+        $("#s-container").css("display","block")
+        $('#s-container h1').addClass('magictime slideLeftReturn');
+        $('#s-container p').addClass('magictime slideRightReturn');
+    });
+
+    // Home menu button
+    $("#home").on("click", () => {
+        $("#h-container").css("display","block")
+        $("#c-container").css("display","none")
+        $("#s-container").css("display","none")
+    });
+
+    // Contact me menu button
+    $("#contact").on("click", () => {
+        $("#h-container").css("display","none")
+        $("#c-container").css("display","block")
+        $("#s-container").css("display","none")
+        $('#c-container p').addClass('magictime puffIn');
+    });
+}
