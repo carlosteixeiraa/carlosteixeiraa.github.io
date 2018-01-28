988416334bef2265cd04247a41f3be2215308d17
carlosteixeiraa <carlosteixeira.email@gmail.com>
2018-01-28 16:39:36 +0000

    Novo site

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+// Animate image and text of home page
+$(document ).ready(() => {
+    $('.me').addClass('magictime puffIn');
+    $('aside h1').addClass('magictime slideLeftReturn');
+    $('.about').addClass('magictime slideRightReturn');
+});	
+
+// Skills menu button
+$("#skills").on("click", () => {
+    $("#h-container").css("display","none")
+    $("#c-container").css("display","none")
+    $("#s-container").css("display","flex")
+    $('#s-container h1').addClass('magictime slideLeftReturn');
+    $('#s-container p').addClass('magictime slideRightReturn');
+});
+
+// Home menu button
+$("#home").on("click", () => {
+    $("#h-container").css("display","flex")
+    $("#c-container").css("display","none")
+    $("#s-container").css("display","none")
+});
+
+// Contact me menu button
+$("#contact").on("click", () => {
+    $("#h-container").css("display","none")
+    $("#c-container").css("display","flex")
+    $("#s-container").css("display","none")
+    $('#c-container p').addClass('magictime puffIn');
+});
